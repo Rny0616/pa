20521bc19f7ba7ca2f3c70ede1a15a59564eaf26
tracer-ics2023 <tracer@njuics.org>
2024-03-06 09:32:29 +0800

    >  compile NEMU 0616 ZZ Linux rny 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:32:29 up 5 days,  1:48,  0 users,  load average: 0.06, 0.14, 0.09

--- ics2023/nemu/tools/gen-expr/gen-expr.c
+++ ics2023/nemu/tools/gen-expr/gen-expr.c
@@ -23,16 +23,65 @@
 // this should be enough
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+// static char *code_format =
+// "#include <stdio.h>\n"
+// "int main() { "
+// "  unsigned result = %s; "
+// "  printf(\"%%u\", result); "
+// "  return 0; "
+// "}";
+
+static char *buf_start = buf;
+static char *buf_end = buf+(sizeof(buf)/sizeof(buf[0]));
+
+static int choose(int n) {
+  return rand() % n;
+}
+
+static void gen_space() {
+  int size = choose(4);
+  if (buf_start < buf_end) {
+    int n_writes = snprintf(buf_start, buf_end-buf_start, "%*s", size, "");
+    if (n_writes > 0) {
+      buf_start += n_writes;
+    }
+  }
+}
+
+static void gen_num() {
+  int num = choose(INT8_MAX);
+  if (buf_start < buf_end) {
+    int n_writes = snprintf(buf_start, buf_end-buf_start, "%d", num);
+    if (n_writes > 0) {
+      buf_start += n_writes;
+    }
+  }
+  gen_space();
+}
+
+static void gen_char(char c) {
+  int n_writes = snprintf(buf_start, buf_end-buf_start, "%c", c);
+  if (buf_start < buf_end) {
+    if (n_writes > 0) {
+      buf_start += n_writes;
+    }
+  }
+}
+
+static char ops[] = {'+', '-', '*', '/'};
+static void gen_rand_op() {
+  int op_index = choose(sizeof(ops));
+  char op = ops[op_index];
+  gen_char(op);
+}
 
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(3))
+  {
+  case 0: gen_num(); break;
+  case 1: gen_char('('); gen_rand_expr(); gen_char(')'); break;
+  default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -45,25 +94,25 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
+    printf("%s\n",buf);
+    // sprintf(code_buf, code_format, buf);
 
-    sprintf(code_buf, code_format, buf);
+    // FILE *fp = fopen("/tmp/.code.c", "w");
+    // assert(fp != NULL);
+    // fputs(code_buf, fp);
+    // fclose(fp);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+    // int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    // if (ret != 0) continue;
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    // fp = popen("/tmp/.expr", "r");
+    // assert(fp != NULL);
 
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
+    // int result;
+    // ret = fscanf(fp, "%d", &result);
+    // pclose(fp);
 
-    int result;
-    ret = fscanf(fp, "%d", &result);
-    pclose(fp);
-
-    printf("%u %s\n", result, buf);
+    // printf("%u %s\n", result, buf);
   }
   return 0;
 }
