2be382da1d65cc3799976aa4458bf239c43f009c
tracer-ics2023 <tracer@njuics.org>
2024-03-06 14:45:44 +0800

    >  compile NEMU 0616 ZZ Linux rny 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:45:44 up 5 days,  5:21,  0 users,  load average: 0.03, 0.08, 0.09

--- ics2023/nemu/src/isa/riscv32/reg.c
+++ ics2023/nemu/src/isa/riscv32/reg.c
@@ -39,10 +39,11 @@ void isa_reg_display()
 
 word_t isa_reg_str2val(const char *s, bool *success)
 {
+  
   int i = 0;
   for (; i < 32; i++)
   {
-    if (strcmp(regs[i],s) == 0)
+    if (strcmp(regs[i],s+1) == 0)
     {
       *success = true;
       return cpu.gpr[i];
