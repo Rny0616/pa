fe6f7d554692d9e0dc48713783600d0b718b09e1
tracer-ics2023 <tracer@njuics.org>
2024-03-14 17:49:49 +0800

    >  compile NEMU 0616 ZZ Linux rny 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:49:49 up 10:11,  0 users,  load average: 0.13, 0.09, 0.05

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,11 +19,23 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction==DIFFTEST_TO_DUT)
+  {
+      memcpy(&buf,&addr,n);
+  }else{
+      memcpy(&addr,&buf,n);
+  }
+  
+  // assert(0);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  // assert(0);
+  if (direction == DIFFTEST_TO_DUT )
+  {
+
+  }
+  
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
